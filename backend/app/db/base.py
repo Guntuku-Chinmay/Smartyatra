17e8aedb0d3ce796383db0c09970e5f6c2279ebb
Guntuku Chinmay <chinmayguntuku@gmail.com>
2026-07-12 11:28:49 +0530

    feat(database): configure PostgreSQL, PostGIS, SQLAlchemy, and Alembic infrastructure

---
+++ backend/app/db/base.py
@@ -0,0 +1,8 @@
+from app.db.base_class import Base
+
+# Import all SQLAlchemy models here.
+# Example:
+#
+# from app.models.city import City
+# from app.models.category import Category
+# from app.models.destination import Destination
